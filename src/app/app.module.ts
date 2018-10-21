cf1fb98cf5bef9c25176dd99ef8b5403693c2d05
William Reilly <wreilly2001@gmail.com>
2018-10-21 07:45:29 -0400

    Module modifications for ngMaterial Schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { WrNavMaterialTwoComponent } from './wr-nav-material-two/wr-nav-material-two.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { WrDashboardMaterialComponent } from './wr-dashboard-material/wr-dashboard-material.component';
+import { WrTableMaterialComponent } from './wr-table-material/wr-table-material.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrNavMaterialTwoComponent,
+    WrDashboardMaterialComponent,
+    WrTableMaterialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
